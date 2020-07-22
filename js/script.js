f99e7f95880c45117fbd62aecb6fd98447a38f84
duho3abp <DanZhidkof@yandex.ru>
2020-07-22 19:00:53 +0300

    add navigation dot for slider

--- js/script.js
+++ js/script.js
@@ -288,20 +288,30 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Слайдер ------------------------------------------------------------------
 
-    const slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    const slider = document.querySelector('.offer__slider'),
+        slidesWrapper = slider.querySelector('.offer__slider-wrapper'),
         slidesField = slidesWrapper.querySelector('.offer__slider-inner'),
         slides = slidesField.querySelectorAll('.offer__slide'),
-        nextBtn = document.querySelector('.offer__slider-next'),
-        prevBtn = document.querySelector('.offer__slider-prev'),
-        current = document.querySelector('#current'),
-        total = document.querySelector('#total'),
-        width = window.getComputedStyle(slidesWrapper).width;
+        nextBtn = slider.querySelector('.offer__slider-next'),
+        prevBtn = slider.querySelector('.offer__slider-prev'),
+        current = slider.querySelector('#current'),
+        total = slider.querySelector('#total'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        carouselIndicators = document.createElement('div');
     let slideIndex = 1,
         offset = 0;
 
-    slides.length < 10 ? total.textContent = `0${slides.length}` : total.textContent = slides.length ;
-    slideIndex < 10 ? current.textContent = `0${slideIndex}` : current.textContent = slideIndex ;
-        
+    const transformSlider = (o, i, dots, dot) => {
+        slidesField.style.transform = `translateX(-${o}px)`;
+        i < 10 ? current.textContent = `0${i}` : current.textContent = i;
+        dots.forEach(dot => dot.style.opacity = '');
+        dot.style.opacity = 1;
+    };
+
+    slides.length < 10 ? total.textContent = `0${slides.length}` : total.textContent = slides.length;
+    slideIndex < 10 ? current.textContent = `0${slideIndex}` : current.textContent = slideIndex;
+
+    slider.style.position = 'relative';
     slidesWrapper.style.overflow = 'hidden';
     slidesField.style.width = `${100 * slides.length}%`;
     slidesField.style.display = 'flex';
@@ -309,9 +319,35 @@ window.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => {
         slide.style.width = width;
     });
+    carouselIndicators.classList.add('carousel-indicators');
+
+    const dots = [];
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('div');
+
+        if (i == slideIndex - 1) {
+            dot.style.opacity = '1';
+        }
+
+        dot.classList.add('dot');
+        dot.setAttribute('id', `${i}`);
+        carouselIndicators.append(dot);
+        dots.push(dot);
+    }
+
+    slider.append(carouselIndicators);
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', () => {
+            offset = dot.id * +width.slice(0, width.length - 2);
+            slideIndex = +dot.id + 1;
+
+            transformSlider(offset, slideIndex, dots, dot);
+        });
+    });
 
     nextBtn.addEventListener('click', () => {
-        
+
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
             slideIndex = 1;
@@ -320,13 +356,12 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
-        slideIndex < 10 ? current.textContent = `0${slideIndex}` : current.textContent = slideIndex ;
+        transformSlider(offset, slideIndex, dots, dots[slideIndex - 1]);
     });
 
     prevBtn.addEventListener('click', () => {
-        
-        if (offset ==  0) {
+
+        if (offset == 0) {
             offset = +width.slice(0, width.length - 2) * (slides.length - 1);
             slideIndex = slides.length;
         } else {
@@ -334,8 +369,7 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`;
-        slideIndex < 10 ? current.textContent = `0${slideIndex}` : current.textContent = slideIndex ;
+        transformSlider(offset, slideIndex, dots, dots[slideIndex - 1]);
     });
 
 });
